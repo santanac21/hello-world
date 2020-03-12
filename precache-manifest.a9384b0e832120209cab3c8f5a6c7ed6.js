29e98ca80b9a37cd8e59cd625f019aeed375126f
Carlos Santana <csantana.antonio@gmail.com>
2020-03-12 19:25:26 -0400

    Updates

---
+++ precache-manifest.a9384b0e832120209cab3c8f5a6c7ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24c6851fa75d62f124bab63b31b85a9",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "2db1a0134d11545abf61",
+    "url": "/hello-world/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ce8521be3c464b0b4",
+    "url": "/hello-world/static/js/2.0d4c82aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hello-world/static/js/2.0d4c82aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db1a0134d11545abf61",
+    "url": "/hello-world/static/js/main.8f58e597.chunk.js"
+  },
+  {
+    "revision": "eca3a178d93bab5c7504",
+    "url": "/hello-world/static/js/runtime-main.30931b85.js"
+  }
+]);
