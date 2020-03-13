43644b35f80c45e2a5e1177248e49b6cd30cd556
Carlos Santana <csantana.antonio@gmail.com>
2020-03-13 11:56:48 -0400

    Added an array of fortunes for the program

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
+import fortuneslist from 'fortune-cookie';
 import './App.css';
 import ButtonCount from './components/ButtonCount'
 
-
+//Learn Blueprint or React Bootstrap
 class App extends Component  {
   render(){
     return (
       // class attribute for HTML is turned into className for React
       <div className="App"> 
-        <ButtonCount name="Carlos" />
+        {/* <ButtonCount name="Carlos" /> */}
+        <p>{fortuneslist.length}</p>
       </div>
     );
   }
